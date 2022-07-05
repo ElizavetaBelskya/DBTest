6beaff672bfc40162ce70a547535eabe41767ee9
ElizavetaBelskya <90205133+ElizavetaBelskya@users.noreply.github.com>
2022-07-05 21:36:48 +0300

    Added

---
+++ app/src/main/java/ru/kpfu/itis/fittrack/data/RecipeDao.kt
@@ -0,0 +1,18 @@
+package ru.kpfu.itis.fittrack.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+@Dao
+interface RecipeDao {
+    @Query("SELECT * from Recipe ORDER BY id_recipe ASC")
+    fun getAllRecipes(): LiveData<List<Recipe>>
+
+//    @Query("SELECT * from Recipe WHERE id_recipe=:id")
+//    fun loadSingle(id: Int): LiveData<Recipe>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun add(recipe: Recipe)
+}
---
+++ app/src/main/java/ru/kpfu/itis/fittrack/fragments/RecipeDescriptionFragment.kt
@@ -0,0 +1,45 @@
+package ru.kpfu.itis.fittrack.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.core.net.toUri
+import androidx.fragment.app.Fragment
+import ru.kpfu.itis.fittrack.R
+import ru.kpfu.itis.fittrack.data.Recipe
+import ru.kpfu.itis.fittrack.data.RecipeDatabase
+import ru.kpfu.itis.fittrack.databinding.FragmentRecipeDescriptionBinding
+
+class RecipeDescriptionFragment : Fragment(R.layout.fragment_recipe_description) {
+    var r: List<Recipe>? = ArrayList<Recipe>()
+    var l: Recipe? = null
+    private var _binding: FragmentRecipeDescriptionBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentRecipeDescriptionBinding.bind(view)
+        val id: Int = arguments?.getInt(ARG_TEXT)?.or(0)!!
+        r = RecipeDatabase.getDatabase(requireContext()).recipeDao().getAllRecipes().value
+        l = r?.get(id)
+        binding.ivPicture.setImageURI((l?.picture)?.toUri())
+        binding.tvCalories.text = "Calories: ${l?.calories}"
+        binding.tvProteins.text = "Proteins: ${l?.proteins}"
+        binding.tvFats.text = "Fats: ${l?.fats}"
+        binding.tvCarbo.text = "Carbohydrates: ${l?.carbohydrates}"
+        binding.tvTitle.text = l?.title
+    }
+
+    companion object {
+        private const val ARG_TEXT = "recipe"
+
+        fun createBundle(id: Int): Bundle {
+            val bundle = Bundle()
+            bundle.putInt(
+                ARG_TEXT,
+                id
+            )
+            return bundle
+        }
+
+    }
+}
